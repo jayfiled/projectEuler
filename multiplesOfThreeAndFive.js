7fe5fb9ed0b7c8d0b4f598f47d7f97c58840df96
Jayfiled <jayfiled@gmail.com>
2018-12-24 23:28:10 +1100

    add problem

--- multiplesOfThreeAndFive.js
+++ multiplesOfThreeAndFive.js
@@ -1,3 +1,9 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 function multThreeFiveCombined(below) {
 	let multThreeAndFive = [];
 	for (let i = 2; i < below; i++) {
@@ -9,3 +15,5 @@ function multThreeFiveCombined(below) {
 		return acc + eachNum;
 	},0)
 }
+
+multThreeFiveCombined(1000); // 233168
